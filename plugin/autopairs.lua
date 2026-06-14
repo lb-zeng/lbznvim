132368b6a6427f1aa9f7bbbb4e525899e5f2c9f3
lbzeng <lbzeng96@gmail.com>
2026-06-14 16:50:41 +0800

    feat(autopairs): add autopair plugin

---
+++ plugin/autopairs.lua
@@ -0,0 +1,9 @@
+vim.pack.add({
+  -- a autopair plugin
+  "https://github.com/windwp/nvim-autopairs",
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  once = true,
+  callback = function() require("nvim-autopairs").setup() end
+})
