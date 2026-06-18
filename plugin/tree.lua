9e61ad90472b39578855a5e731ec198e21ee8ab4
lbzeng <lbzeng96@gmail.com>
2026-06-18 21:46:33 +0800

    feat(tree): add plugin nvim-tree

---
+++ plugin/tree.lua
@@ -0,0 +1,21 @@
+vim.pack.add({
+    -- a file explorer
+    "https://github.com/nvim-tree/nvim-tree.lua",
+    -- provides Nerd Font icons (glyphs)
+    "https://github.com/nvim-tree/nvim-web-devicons",
+})
+
+  -- disable netrw at the very start of your init.lua
+  vim.g.loaded_netrw = 1
+  vim.g.loaded_netrwPlugin = 1
+
+  -- optionally enable 24-bit colour
+  vim.opt.termguicolors = true
+
+  require("nvim-tree").setup()
+
+local keymap = vim.keymap -- for conciseness
+keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" }) -- toggle file explorer
+keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle File Explorer on Current File" }) -- toggle file explorer on current file
+keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse File Explorer" }) -- collapse file explorer
+keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh File Explorer" }) -- refresh file explorer
